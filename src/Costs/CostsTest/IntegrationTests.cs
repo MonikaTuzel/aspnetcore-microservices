421c54e49aacc6f7c3bf53f60b43ea6fec5f08f7
Monika Tuzel <monika.tuzel@gmail.com>
2021-05-15 14:13:18 +0200

    Create CostTest

---
+++ src/Costs/CostsTest/IntegrationTests.cs
@@ -0,0 +1,163 @@
+using CostsApi;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using CostsApi.Models;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+//using System.Text.Json;
+
+namespace CostsTests
+{
+    public class IntegrationTests : IClassFixture<TestFixture<Startup>>
+    {
+
+        private HttpClient Client;
+        private string requestUrl = "/api/Costs";
+
+        public IntegrationTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task TestGetAllAsync()
+        {
+            // Act
+            var response = await Client.GetAsync(requestUrl);
+            string jsonString = response.Content.ReadAsStringAsync().Result;
+            var act = JsonConvert.DeserializeObject<List<Costs>>(jsonString);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(act.Count > 0);
+        }
+
+        [Fact]
+        public async Task TestGetAsync()
+        {
+            // Act
+            var response = await Client.GetAsync(requestUrl + "/1");
+
+            string jsonString = response.Content.ReadAsStringAsync().Result;
+            var act = JsonConvert.DeserializeObject<Costs>(jsonString);
+
+            // Assert
+            Assert.Equal(1, act.idCosts);
+            Assert.Equal("Tankowanie", act.Description);
+        }
+        [Fact]
+        public async Task TestPostItem()
+        {
+            // Dodanie nowego wiersza
+            var requestBody = new
+            {
+                Url = requestUrl,
+                Body = new Costs
+                {
+                    idCosts = 10,
+                    Description = "Nowy opis"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(requestBody.Url, ContentHelper.GetStringContent(requestBody.Body));
+
+            var response2 = await Client.GetAsync(requestUrl + "/10");
+            string jsonString = response2.Content.ReadAsStringAsync().Result;
+            var act = JsonConvert.DeserializeObject<Costs>(jsonString);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.Equal(requestBody.Body.idCosts, act.idCosts);
+            Assert.Equal(requestBody.Body.Description, act.Description);
+        }
+
+        [Fact]
+        public async Task TestPutItemAsync()
+        {
+            // Update nowego wiersza
+            var requestBody = new
+            {
+                Url = requestUrl + "/10",
+                Body = new
+                {
+                    idCosts = 10,
+                    Description = "Update opisu"
+                }
+            };
+
+            // Act
+            var response = await Client.PutAsync(requestBody.Url, ContentHelper.GetStringContent(requestBody.Body));
+
+            var response2 = await Client.GetAsync(requestUrl + "/10");
+            string jsonString = response2.Content.ReadAsStringAsync().Result;
+            var act = JsonConvert.DeserializeObject<Costs>(jsonString);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.Equal(requestBody.Body.idCosts, act.idCosts);
+            Assert.Equal(requestBody.Body.Description, act.Description);
+        }
+
+        [Fact]
+        public async Task TestDeleteItemAsync()
+        {
+            var response = await Client.DeleteAsync(requestUrl + "/10");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var response2 = await Client.GetAsync(requestUrl + "/10");
+            //USTAWIC SPRAWDZANIE
+            //  Assert.False(singleResponse.Id);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(response2.IsSuccessStatusCode);
+
+        }
+
+        [Fact]
+        public async Task Return_404_Result()
+        {
+            // Act
+            var response = await Client.GetAsync(String.Empty);
+
+            // Assert
+            //response.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        [Fact]
+        public async Task Return_400_Result()
+        {
+            // Act
+            var response = await Client.DeleteAsync(requestUrl + "/{id_url}"); 
+
+            // Assert
+            //response.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+        [Fact]
+        public async Task Return_500_Result()
+        {
+            // Act
+            var response = await Client.DeleteAsync(requestUrl + "/0");
+
+            // Assert
+            //response.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+        }
+    }
+}
